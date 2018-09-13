29dd7ba17b226805b2ca18c3bd9880787884c02d
Mikal Whaley <mwhaley@csumb.edu>
2018-09-13 05:44:29 +0000

    Added Homework1

---
+++ practice/php/test.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+
+$n = 20943;
+$n = number_format($n,2); 
+echo $n  . "<br><br>";
+
+$n = rand(5,15);   
+echo $n  . "<br><br>";
+
+$n = "hElLo WoRlD!";
+echo strtoupper($n)  .  "<br><br>";
+
+?>
+
+</html>
