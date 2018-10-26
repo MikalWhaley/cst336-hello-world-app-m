29d7bac97b7236cae7b76742105e9a9061f3e9d8
Mikal Whaley <mwhaley@csumb.edu>
2018-10-26 20:02:19 +0000

    added database

--- labs/meme_lab.php/database.php
+++ labs/meme_lab.php/database.php
@@ -3,10 +3,21 @@
 // connect to our mysql database server
 
 function getDatabaseConnection() {
+    
+    
+    if($_SERVER['SEVER_NAME'] =="mikal-cst336-mikal.c9users.io"){
     $host = "localhost";
     $username = "Mikal";
     $password = "847Themikeman"; // best practice: define this in a separte file
     $dbname = "meme_lab"; 
+    
+    }else{
+        $host = "us-cdbr-iron-east-01.cleardb.net";
+    $username = "baa5d52535b2e7";
+    $password = "99df1e31"; // best practice: define this in a separte file
+    $dbname = "heroku_b659feb23887a6f"; 
+        
+    }
     
     // Create connection
     $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
@@ -17,6 +28,6 @@ function getDatabaseConnection() {
 
 // temporary test code
 //$dbConn = getDatabaseConnection(); 
-
+ 
 
 ?>
